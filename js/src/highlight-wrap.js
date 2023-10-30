f40daeb544644419807d309943fb02652a62a978
qcmoke <1667164190@qq.com>
2023-10-30 19:06:58 +0800

    Site updated: 2023-10-30 19:06:29

--- js/src/highlight-wrap.js
+++ js/src/highlight-wrap.js
@@ -1,3 +1,53 @@
-// build time:Sat Oct 28 2023 04:01:57 GMT+0800 (GMT+08:00)
-function initMacPanel(){var a=$(".highlight").not(".gist .highlight");for(var t=0;t<a.length;t++){var e=$(a[t]);var i=e.attr("class");var r=/highlight(\s?)([a-zA-Z]*)/;r.test(i);var n=RegExp.$2||"plain";var o=$("<div>").attr("data-rel",n.toUpperCase()).addClass("highlight-wrap");var c=e.wrap(o);c.before('<button class="copy-btn" data-clipboard-snippet=""><i class="fa fa-clipboard"></i></button>')}}function initClipboard(){var a=new ClipboardJS(".copy-btn",{target:function(a){var t=a.parentElement;var e=t.querySelector(".code");return e}});a.on("success",function(a){var t=a.trigger;var e=t.querySelector("i");e.className="fa fa-check";t.onmouseleave=function(t){a.clearSelection();setTimeout(function(){e.className="fa fa-clipboard"},1e3)}})}$(document).ready(function(){initMacPanel();initClipboard()});
-//rebuild by neat 
+/**
+ * @author qcmoke
+ * @description "mac面板"和"代码全选复制"
+ * @Dependencies https://github.com/zenorocha/clipboard.js
+ * @version 2.0
+ */
+
+function initMacPanel() {
+  var highlights = $(".highlight").not(".gist .highlight");
+  for (var i = 0; i < highlights.length; i++) {
+    var figure = $(highlights[i]);
+    var css = figure.attr("class");
+    var patt = /highlight(\s?)([a-zA-Z]*)/;
+    patt.test(css);
+    //得到第二个小括号的内容，如果为空，则设置为plain
+    var language = RegExp.$2 || "plain";
+    var newDiv = $("<div>")
+      .attr("data-rel", language.toUpperCase())
+      .addClass("highlight-wrap");
+    var wrap = figure.wrap(newDiv);
+    wrap.before(
+      '<button class="copy-btn" data-clipboard-snippet=""><i class="fa fa-clipboard"></i></button>'
+    );
+  }
+}
+
+function initClipboard() {
+  var clipboard = new ClipboardJS(".copy-btn", {
+    target: function(trigger) {
+      //return trigger.nextElementSibling;//返回复制按钮下一个列表选项的 HTML 内容
+      ///返回复制按钮所在dom下含有class为code的第一个dom对象(可以防止复制figcaption的内容)
+      var wrap = trigger.parentElement;
+      var code = wrap.querySelector(".code");
+      return code;
+    }
+  });
+  clipboard.on("success", function(e) {
+    var button = e.trigger;
+    var iElement = button.querySelector("i");
+    iElement.className = "fa fa-check";
+    button.onmouseleave = function(ev) {
+      e.clearSelection();
+      setTimeout(function() {
+        iElement.className = "fa fa-clipboard";
+      }, 1000);
+    };
+  });
+}
+
+$(document).ready(function() {
+  initMacPanel();
+  initClipboard();
+});
